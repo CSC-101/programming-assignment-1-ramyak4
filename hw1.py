3fb03875a929c7790a463ab4fc32e7804233a944
Ramya Kaja <ramyasree.kaja@gmail.com>
2024-10-23 22:10:36 -0700

    Programming Assignment 1 finished

--- hw1.py
+++ hw1.py
@@ -1,28 +1,104 @@
-import data
+from data import Price, Rectangle, Point, Book, Circle, Employee
+
+import math
 
 # Write your functions for each part in the space below.
 
 # Part 1
-
+#Counts the number of vowels in a given string
+#input: a string
+#output: an integer that sums up the number of vowels
+def vowel_count(vowel: str) -> int:
+    possible = "aeiouAEIOU"
+    count = 0
+    for char in vowel:
+        if char in possible:
+            count += 1
+    return count
 
 # Part 2
-
+#Creates a list of all the short lists
+#input: a nested list with lists of integers
+#output: a nested list with lists of integers
+def short_lists(list1: list[list[int]]) -> list[list[int]]:
+    short = []
+    for nList in list1:
+        if len(nList) == 2:
+            short.append(nList)
+    return short
 
 # Part 3
-
+#Reorder the short lists in a nested list to make them in ascending order
+#input: a nested list with lists of integers
+#output: a nested list with lists of integers
+def ascending_pairs(list1: list[list[int]]) -> list[list[int]]:
+    result = []
+    for nlist in list1:
+        if len(nlist) == 2:
+            if nlist[0] <= nlist[1]:
+                result.append(nlist)
+            else:
+                nlist = [nlist[1], nlist[0]]
+                result.append(nlist)
+        else:
+            result.append(nlist)
+    return result
 
 # Part 4
-
+#Calculates the total price given two prices
+#input: two Price objects
+#output: Price object
+def add_prices(price1: Price, price2: Price) -> Price:
+    total_dollars = price1.dollars + price2.dollars
+    total_cents = price1.cents + price2.cents
+    if total_cents > 99:
+        total_dollars += total_cents // 100
+        total_cents = total_cents % 100
+    return Price(total_dollars, total_cents)
 
 # Part 5
+#Calculates the area of a rectangle
+#input: One rectangle object
+#output: an integer for the total area calculation
+def rectangle_area (rec1:Rectangle) -> int:
+    width = rec1.bottom_right.x - rec1.top_left.x
+    height = rec1.top_left.y - rec1.bottom_right.y
 
+    return int(width * height)
 
 # Part 6
-
+#Creates a list of all the books in a list written by a certain author
+#input: a string for the author name, a list of books
+#outout: a list of books written by the author
+def books_by_author(author:str, list1: list[Book]) -> list[Book]:
+    return [book for book in list1 if author in book.authors]
 
 # Part 7
+#Creates the smallest possible circle that bounds a rectangle
+#input: a rectangle object
+#output: a circle object
+def circle_bound(rec1: Rectangle) -> Circle:
+    center_x = (rec1.top_left.x + rec1.bottom_right.x)/2
+    center_y = (rec1.top_left.y + rec1.bottom_right.y)/2
+    center = Point(center_x, center_y)
 
+    radius = math.sqrt(((center_x - rec1.top_left.x) ** 2) + (center_y - rec1.top_left.y) ** 2)
+
+    return Circle(center, radius)
 
 # Part 8
+#Creates a list of all the employees that are paid less than the average of all employees
+#input: a list of all employees
+#output: a list of all employees paid less than average
+def below_pay_average(list1: list[Employee]) -> list[str]:
+
+    if not list1:
+        return []
+
+    total_pay = sum(employee.pay_rate for employee in list1)
+    average_pay = total_pay/len(list1)
+    below_average = [employee.name for employee in list1 if employee.pay_rate < average_pay]
+    return below_average
+
 
 
--- hw1_test.py
+++ hw1_test.py
@@ -1,36 +1,92 @@
-import data
-import hw1
+from Point import Point
+from data import Price, Rectangle, Circle, Book, Employee
 import unittest
+import math
+
+from hw1 import vowel_count, short_lists, ascending_pairs, add_prices, rectangle_area, circle_bound, books_by_author, \
+    below_pay_average
 
 
 # Write your test cases for each part below.
 
 class TestCases(unittest.TestCase):
     # Part 1
-
+    def test_vowel_count(self):
+        input_list = "Hello World"
+        result = vowel_count(input_list)
+        expected = 3
+        self.assertEqual(expected, result)
 
     # Part 2
-
+    def test_short_lists(self):
+        input_list = [[1, 2], [3], [4, 5, 6], [8, 7]]
+        result = short_lists(input_list)
+        expected = [[1,2], [8,7]]
+        self.assertEqual(expected, result)
 
     # Part 3
-
+    def test_ascending_pairs(self):
+        input_list = [[1, 2], [3], [4, 5, 6], [8, 7]]
+        result = ascending_pairs(input_list)
+        expected = [[1, 2], [3], [4, 5, 6], [7, 8]]
+        self.assertEqual(expected, result)
 
     # Part 4
-
+    def test_add_prices(self):
+        price1 = Price(5, 75)
+        price2 = Price(3, 50)
+        price3 = add_prices(price1, price2)
+        expected = Price(9,25)
+        self.assertEqual(expected, price3)
 
     # Part 5
-
+    def test_rectangle_area(self):
+        top_left = Point(1,4)
+        bottom_right = Point(5,1)
+        rec1 = Rectangle(top_left, bottom_right)
+        result = rectangle_area(rec1)
+        expected = 12
+        self.assertEqual(expected, result)
 
     # Part 6
+    def test_books_by_author(self):
+        book1 = Book(["J.K. Rowling"], "Harry Potter and the Goblets Fire")
+        book2 = Book(["J.K. Rowling"], "Harry Potter and the Deathly Hallows")
+        book3 = Book(["Rick Riordan"], "Percy Jackson and the Lightning Thief")
+        book4 = Book(["Rick Riordan"], "Percy Jackson and the Last Olympian")
 
+        list1 = [book1, book2, book3, book4]
+
+        rowling = books_by_author("J.K. Rowling", list1)
+        expected_rowling = [Book(['J.K. Rowling'], 'Harry Potter and the Goblets Fire'), Book(['J.K. Rowling'], 'Harry Potter and the Deathly Hallows')]
+        self.assertEqual(expected_rowling, rowling)
+
+        riordan = books_by_author("Rick Riordan", list1)
+        expected_riordan = [Book(['Rick Riordan'], 'Percy Jackson and the Lightning Thief'),
+                            Book(['Rick Riordan'], 'Percy Jackson and the Last Olympian')]
+        self.assertEqual(expected_riordan, riordan)
 
     # Part 7
-
+    def test_circle_bound(self):
+        rec1 = Rectangle(Point(1, 5), Point(4, 1))
+        bounding_circle = circle_bound(rec1)
+        center_x = (1+4) / 2
+        center_y = (5+1) / 2
+        expected_circle = Point(center_x, center_y)
+        radius = math.sqrt((center_x-1) **2 + (center_y-5) **2)
+        expected = Circle(expected_circle,radius)
+        self.assertEqual(expected, Circle(expected_circle, radius))
 
     # Part 8
-
-
-
+    def test_below_pay_average(self):
+        employees = [
+            Employee("Ramya", 5000),
+            Employee("Kamala", 4500),
+            Employee("Heather", 5500),
+            Employee("Erik", 4700)
+        ]
+        result = [below_pay_average(employees)]
+        expected = ['Kamala', 'Erik']
 
 
 if __name__ == '__main__':
